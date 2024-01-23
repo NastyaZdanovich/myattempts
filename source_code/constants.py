5da0bfc58922235bd98634ff02d901086bb4d80e
NastyaZdanovich <7nastya17@mail.ru>
2024-01-23 08:25:05 +0300

    first attempt

---
+++ source_code/constants.py
@@ -0,0 +1,17 @@
+import random
+
+
+class Host:
+    OZ = 'https://oz.by/'
+
+
+class Info:
+    EMAIL = 'ivan_iv_vi_ivanov@mail.ru'
+    PASSWORD = 'pr)8PFToisP7'
+    PHONENUMBER = str(random.randint(290000000, 299999999))
+
+
+class Search:
+    BRAND = 'Kaaral'
+    ITEMNUMBER = '10773581'
+
---
+++ tests/test_registration_page.py
@@ -0,0 +1,25 @@
+import allure
+from source_code.constants import Info
+from source_code.pages.sign_in_page import SignInPage
+
+
+class TestRegistration:
+    def test_log_in_process(self, driver, main_page):
+        with allure.step("1.Нажать кнопку 'Войти'"):
+            main_page.click_by_xpath(main_page.sign_in_xpath)
+
+        with allure.step("2. Войти с помощью своего профиля Mail.ru "):
+            sign_in_page = SignInPage(driver)
+            sign_in_page.click_by_xpath(sign_in_page.email_registration)
+
+        # with allure.step("3.Заполнить поля 'Электронная почта' и 'Пароль'"):
+        #     sign_in_page.switch_handler(-1)
+        #     sign_in_page.fill_in_a_form(sign_in_page.confirm_email_xpath, Info.EMAIL)
+        #     sign_in_page.fill_in_a_form(sign_in_page.confirm_password_xpath, Info.PASSWORD)
+        #     sign_in_page.click_by_xpath(sign_in_page.enter)
+        #
+        # with allure.step("4. Проверить, что вход в кабинет произошел "):
+        #
+        #     main_page.switch_handler(0)
+        #     text = main_page.get_a_text(main_page.my_oz_xpath)
+        #     assert text == 'Мой OZ', f"Expected text 'Мой OZ', actual text {text}"

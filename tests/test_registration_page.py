7000787935ef8477f8c422cd25067706ef8c1cd5
NastyaZdanovich <7nastya17@mail.ru>
2024-01-23 08:47:39 +0300

    first attempt

--- tests/test_registration_page.py
+++ tests/test_registration_page.py
@@ -17,7 +17,7 @@ class TestRegistration:
             sign_in_page.fill_in_a_form(sign_in_page.confirm_email_xpath, Info.EMAIL)
             sign_in_page.fill_in_a_form(sign_in_page.confirm_password_xpath, Info.PASSWORD)
             sign_in_page.click_by_xpath(sign_in_page.enter)
-        #
+
         # with allure.step("4. Проверить, что вход в кабинет произошел "):
         #
         #     main_page.switch_handler(0)
